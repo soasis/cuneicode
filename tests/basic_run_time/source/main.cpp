7bd4fba07e80a426c9d00f26bdd86138c72f5b77
ThePhD <phdofthehouse@gmail.com>
2022-04-16 22:57:15 -0400

    💚 Show the names of the encodings for the tests

--- tests/basic_run_time/source/main.cpp
+++ tests/basic_run_time/source/main.cpp
@@ -32,21 +32,17 @@
 
 #include <ztd/cuneicode/tests/utf8_startup.hpp>
 
-#include <ztd/idk/encoding_name.hpp>
+#include <ztd/idk/encoding_detection.hpp>
 
 #include <iostream>
 #include <clocale>
 
 int main(int argc, char* argv[]) {
 	std::cout << "=== Encoding Names ===" << std::endl;
-	std::cout << "Literal Encoding: "
-	          << ztd::__idk_detail::to_name(ztd::__idk_detail::__to_encoding_id(
-	                  ZTD_CXX_COMPILE_TIME_ENCODING_NAME_GET_I_()))
-	          << std::endl;
-	std::cout << "Wide Literal Encoding: "
-	          << ztd::__idk_detail::to_name(ztd::__idk_detail::__to_encoding_id(
-	                  ZTD_CXX_COMPILE_TIME_WIDE_ENCODING_NAME_GET_I_()))
-	          << std::endl;
+	std::cout << "Literal Encoding: " << ztd::literal_encoding_name() << std::endl;
+	std::cout << "Wide Literal Encoding: " << ztd::wide_literal_encoding_name() << std::endl;
+	std::cout << "Execution Encoding: " << ztd::execution_encoding_name() << std::endl;
+	std::cout << "Wide Execution Encoding: " << ztd::wide_execution_encoding_name() << std::endl;
 	int result = Catch::Session().run(argc, argv);
 	return result;
 }
